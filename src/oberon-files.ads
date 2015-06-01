4b33e962f655716aca973ea84b511e7857528480
Luke A. Guest <laguest@archeia.com>
2015-06-01 16:43:06 +0100

    Removed unnecessary string code.

--- src/oberon-files.ads
+++ src/oberon-files.ads
@@ -4,7 +4,7 @@
 --  Oberon.Files
 --  This is the root package for file handling.
 package Oberon.Files is
-   subtype File is String; -- (Positive range <>);
+   subtype File is String;
 
    function Open (File_Name : String) return File;
 end Oberon.Files;
