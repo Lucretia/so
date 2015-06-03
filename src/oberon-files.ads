3f7e6149ee6f07a473bcef5d7612a2d86000b96b
Luke A. Guest <laguest@archeia.com>
2015-06-03 17:49:29 +0100

    Added an access type to the file type so we can allocate one dynamically.

--- src/oberon-files.ads
+++ src/oberon-files.ads
@@ -5,6 +5,7 @@
 --  This is the root package for file handling.
 package Oberon.Files is
    subtype File is String;
+   type File_Access is access all File;
 
    function Open (File_Name : String) return File;
 end Oberon.Files;
